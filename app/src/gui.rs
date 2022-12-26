1291bfd8534d3ffc7ad6e0c26e61c89b5bb5f767
Raman Fedaseyeu <ramanf@MBNL0188.local>
2022-12-26 11:04:32 +0100

    Extract GUI into a separate file

---
+++ app/src/gui.rs
@@ -0,0 +1,162 @@
+use imgui::{sys::ImVec2, ProgressBar};
+use imgui_glow_renderer::glow::{self, HasContext};
+
+use conf::Conf;
+use core::{app_state::AppState, timer};
+use sdl2::{
+    sys::SDL_WindowFlags,
+    video::{GLProfile, SwapInterval},
+};
+
+pub struct Gui {
+    _gl_context: sdl2::video::GLContext,
+    event_pump: sdl2::EventPump,
+    platform: imgui_sdl2_support::SdlPlatform,
+    imgui: imgui::Context,
+    window: sdl2::video::Window,
+    window_height: f32,
+    renderer: imgui_glow_renderer::AutoRenderer,
+}
+
+impl Gui {
+    pub fn new(conf: &Conf, app_state: &AppState) -> Self {
+        let sdl = sdl2::init().unwrap();
+        let sdl_video = sdl.video().unwrap();
+
+        let gl_attr = sdl_video.gl_attr();
+        gl_attr.set_context_version(3, 3);
+        gl_attr.set_context_profile(GLProfile::Core);
+
+        let mut window = sdl_video
+            .window(&conf.title, conf.window_width, 240)
+            .set_window_flags(SDL_WindowFlags::SDL_WINDOW_ALWAYS_ON_TOP as u32)
+            // Borderless looks good, but requires extra code
+            // to enable the user to move the window.
+            // .borderless()
+            .allow_highdpi()
+            .opengl()
+            .position_centered()
+            .build()
+            .unwrap();
+
+        let gl_context = window.gl_create_context().unwrap();
+        window.gl_make_current(&gl_context).unwrap();
+
+        window
+            .subsystem()
+            .gl_set_swap_interval(SwapInterval::VSync)
+            .unwrap();
+
+        let gl = glow_context(&window);
+
+        let mut imgui = imgui::Context::create();
+        imgui.set_ini_filename(None);
+        imgui.set_log_filename(None);
+
+        imgui
+            .fonts()
+            .add_font(&[imgui::FontSource::DefaultFontData { config: None }]);
+
+        let window_padding: ImVec2 = imgui.style().window_padding.into();
+        let item_spacing: ImVec2 = imgui.style().item_spacing.into();
+        let window_height = conf.bar_height as f32 * app_state.timers.len() as f32
+            + item_spacing.y * (app_state.timers.len() - 1) as f32
+            + window_padding.y * 2.0;
+
+        window
+            .set_size(conf.window_width, window_height as u32)
+            .unwrap();
+
+        let platform = imgui_sdl2_support::SdlPlatform::init(&mut imgui);
+        let renderer = imgui_glow_renderer::AutoRenderer::initialize(gl, &mut imgui).unwrap();
+        let event_pump = sdl.event_pump().unwrap();
+
+        Self {
+            _gl_context: gl_context,
+            event_pump,
+            platform,
+            imgui,
+            window,
+            window_height,
+            renderer,
+        }
+    }
+
+    pub fn render(&mut self, conf: &Conf, app_state: &mut AppState) -> Result<(), ()> {
+        for event in self.event_pump.poll_iter() {
+            self.platform.handle_event(&mut self.imgui, &event);
+
+            if let sdl2::event::Event::Quit { .. } = event {
+                return Err(());
+            }
+        }
+
+        self.platform
+            .prepare_frame(&mut self.imgui, &self.window, &self.event_pump);
+
+        let ui = self.imgui.new_frame();
+
+        ui.window(&conf.title)
+            .no_decoration()
+            .position([0.0, 0.0], imgui::Condition::Once)
+            .size(
+                [conf.window_width as f32, self.window_height],
+                imgui::Condition::Once,
+            )
+            .build(|| {
+                for i in 0..app_state.timers.len() {
+                    let t = &mut app_state.timers[i];
+
+                    let t_id = ui.push_id_usize(i);
+
+                    ProgressBar::new(t.elapsed_frac())
+                        .size([conf.window_width as f32 - 62.0, conf.bar_height as f32])
+                        .overlay_text(t.label())
+                        .build(ui);
+
+                    ui.same_line();
+                    match t.state() {
+                        timer::State::Stopped => {
+                            if ui.button(">") {
+                                t.start()
+                            }
+                        }
+                        timer::State::Started => {
+                            if ui.button("_") {
+                                t.pause();
+                            }
+                        }
+                        timer::State::Paused => {
+                            if ui.button(">") {
+                                t.resume();
+                            }
+                        }
+                    }
+
+                    ui.same_line();
+                    ui.disabled(t.state() == timer::State::Stopped, || {
+                        if ui.button("X") {
+                            t.stop();
+                        }
+                    });
+
+                    t_id.end();
+                }
+            });
+
+        let draw_data = self.imgui.render();
+
+        unsafe { self.renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
+        self.renderer.render(draw_data).unwrap();
+
+        self.window.gl_swap_window();
+
+        Ok(())
+    }
+}
+
+fn glow_context(window: &sdl2::video::Window) -> glow::Context {
+    unsafe {
+        glow::Context::from_loader_function(|s| window.subsystem().gl_get_proc_address(s) as _)
+    }
+}
--- app/src/main.rs
+++ app/src/main.rs
@@ -3,29 +3,12 @@ use std::io;
 use std::str::FromStr;
 use std::time::Duration;
 
-use glow::HasContext;
-use imgui::sys::ImVec2;
-use imgui::Condition;
-use imgui::Context;
-use imgui::ProgressBar;
-use imgui_glow_renderer::glow;
-use imgui_glow_renderer::AutoRenderer;
-use imgui_sdl2_support::SdlPlatform;
-use sdl2::sys::SDL_WindowFlags;
-use sdl2::video::SwapInterval;
-use sdl2::{
-    event::Event,
-    video::{GLProfile, Window},
-};
-
 use conf::Conf;
-use core::{app_state::AppState, timer};
+use core::app_state::AppState;
 
-fn glow_context(window: &Window) -> glow::Context {
-    unsafe {
-        glow::Context::from_loader_function(|s| window.subsystem().gl_get_proc_address(s) as _)
-    }
-}
+mod gui;
+
+use gui::Gui;
 
 fn load_conf() -> io::Result<Conf> {
     let data = read_to_string("tomato.toml")?;
@@ -40,128 +23,21 @@ fn main() {
     let mut app_state = AppState {
         timers: conf
             .timers
-            .into_iter()
+            .iter()
             .map(|t| {
-                core::timer::Timer::new(t.label, Duration::from_secs_f32(t.duration as f32 * 60.0))
+                core::timer::Timer::new(
+                    t.label.clone(),
+                    Duration::from_secs_f32(t.duration as f32 * 60.0),
+                )
             })
             .collect::<Vec<_>>(),
     };
 
-    let sdl = sdl2::init().unwrap();
-    let sdl_video = sdl.video().unwrap();
+    let mut gui = Gui::new(&conf, &app_state);
 
-    /* Hint SDL to initialize an OpenGL 3.3 core profile context */
-    let gl_attr = sdl_video.gl_attr();
-    gl_attr.set_context_version(3, 3);
-    gl_attr.set_context_profile(GLProfile::Core);
-
-    let mut window = sdl_video
-        .window(&conf.title, conf.window_width, 240)
-        .set_window_flags(SDL_WindowFlags::SDL_WINDOW_ALWAYS_ON_TOP as u32)
-        .allow_highdpi()
-        .opengl()
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let gl_context = window.gl_create_context().unwrap();
-    window.gl_make_current(&gl_context).unwrap();
-
-    window
-        .subsystem()
-        .gl_set_swap_interval(SwapInterval::VSync)
-        .unwrap();
-
-    let gl = glow_context(&window);
-
-    let mut imgui = Context::create();
-    imgui.set_ini_filename(None);
-    imgui.set_log_filename(None);
-
-    imgui
-        .fonts()
-        .add_font(&[imgui::FontSource::DefaultFontData { config: None }]);
-
-    let window_padding: ImVec2 = imgui.style().window_padding.into();
-    let item_spacing: ImVec2 = imgui.style().item_spacing.into();
-    let window_height = conf.bar_height as f32 * app_state.timers.len() as f32
-        + item_spacing.y * (app_state.timers.len() - 1) as f32
-        + window_padding.y * 2.0;
-
-    window
-        .set_size(conf.window_width, window_height as u32)
-        .unwrap();
-
-    let mut platform = SdlPlatform::init(&mut imgui);
-    let mut renderer = AutoRenderer::initialize(gl, &mut imgui).unwrap();
-    let mut event_pump = sdl.event_pump().unwrap();
-
-    'main: loop {
-        for event in event_pump.poll_iter() {
-            platform.handle_event(&mut imgui, &event);
-
-            if let Event::Quit { .. } = event {
-                break 'main;
-            }
+    loop {
+        if gui.render(&conf, &mut app_state).is_err() {
+            break;
         }
-
-        platform.prepare_frame(&mut imgui, &window, &event_pump);
-
-        let ui = imgui.new_frame();
-
-        ui.window(&conf.title)
-            .no_decoration()
-            .position([0.0, 0.0], Condition::Always)
-            .size(
-                [conf.window_width as f32, window_height],
-                Condition::FirstUseEver,
-            )
-            .build(|| {
-                for i in 0..app_state.timers.len() {
-                    let t = &mut app_state.timers[i];
-
-                    let t_id = ui.push_id_usize(i);
-
-                    ProgressBar::new(t.elapsed_frac())
-                        .size([conf.window_width as f32 - 62.0, conf.bar_height as f32])
-                        .overlay_text(t.label())
-                        .build(ui);
-
-                    ui.same_line();
-                    match t.state() {
-                        timer::State::Stopped => {
-                            if ui.button(">") {
-                                t.start()
-                            }
-                        }
-                        timer::State::Started => {
-                            if ui.button("_") {
-                                t.pause();
-                            }
-                        }
-                        timer::State::Paused => {
-                            if ui.button(">") {
-                                t.resume();
-                            }
-                        }
-                    }
-
-                    ui.same_line();
-                    ui.disabled(t.state() == timer::State::Stopped, || {
-                        if ui.button("X") {
-                            t.stop();
-                        }
-                    });
-
-                    t_id.end();
-                }
-            });
-
-        let draw_data = imgui.render();
-
-        unsafe { renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
-        renderer.render(draw_data).unwrap();
-
-        window.gl_swap_window();
     }
 }
